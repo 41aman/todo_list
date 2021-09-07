8a383be0733b148cb0a128d8541f7b0541342216
41aman <41aman.p@gmail.com>
2021-09-07 20:14:24 +0530

    Added firestore dependencies
    Modified project-level, and app-level build.gradle files and pubspec.yaml file.
    Updated main.dart to test Firebase connection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
