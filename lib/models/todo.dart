c767aae098c1897fc57516e0607ac73213ea73d2
41aman <41aman.p@gmail.com>
2021-09-29 03:08:56 +0530

    todo model: added an attribute for DocumentSnapshot
    This will be used to implement pagination.

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,14 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Todo {
   String id;
   String title;
   String desc;
   bool completed;
+  DocumentSnapshot? documentSnapshot;
 
   Todo({
     required this.title,
     this.desc = '',
     this.completed = false,
     this.id = '',
+    this.documentSnapshot = null,
   });
 
   void setTitle(String title) {
