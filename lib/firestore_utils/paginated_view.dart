54b21762d7103ec5c04f6af9d6546b6cad14587f
41aman <41aman.p@gmail.com>
2021-09-29 03:13:54 +0530

    Created paginated_view to implement pagination.
    setPaginatedViewState is used to set the state after a todo widget is either toggled or removed.

--- lib/firestore_utils/paginated_view.dart
+++ lib/firestore_utils/paginated_view.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/models/todo.dart';
-import 'package:todo_list/screens/todolist/todowidget.dart';
+import 'package:todo_list/screens/todowidget.dart';
 
 class PaginatedList extends StatefulWidget {
   final bool completed;
@@ -9,6 +9,24 @@ class PaginatedList extends StatefulWidget {
   static FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   static CollectionReference _todos =
       PaginatedList.firebaseFirestore.collection('todos');
+  static final List<Todo> todoList = [];
+
+  static void addTodo(Todo todo) => todoList.insert(0, todo);
+
+  static void editTodo(Todo todo) {
+    int index = todoList.indexWhere((element) => element.id == todo.id);
+    todoList[index].title = todo.title;
+    todoList[index].desc = todo.desc;
+  }
+
+  static void removeTodo(String id) {
+    todoList.removeWhere((element) => element.id == id);
+  }
+
+  static void toggleTodo(String id) {
+    int index = todoList.indexWhere((element) => element.id == id);
+    todoList[index].completed = !(todoList[index].completed);
+  }
 
   @override
   _PaginatedListState createState() => _PaginatedListState();
@@ -19,7 +37,6 @@ class _PaginatedListState extends State<PaginatedList>
   static const int limit = 8;
   final ScrollController _scrollController = ScrollController();
 
-  final List<Todo> todoList = [];
   DocumentSnapshot? lastTodo;
   DocumentSnapshot? lastComplete;
 
@@ -27,7 +44,17 @@ class _PaginatedListState extends State<PaginatedList>
   bool hasMore = true;
 
   getTodos() async {
-    print('test');
+    print('get todos');
+    List<Todo> todos = PaginatedList.todoList
+        .where((element) => element.completed == false)
+        .toList();
+    List<Todo> completedTodos = PaginatedList.todoList
+        .where((element) => element.completed == true)
+        .toList();
+    lastTodo = todos.length == 0 ? null : todos.last.documentSnapshot;
+    lastComplete = completedTodos.length == 0
+        ? null
+        : completedTodos.last.documentSnapshot;
     if (!hasMore) return;
     if (isLoading) return;
     setState(() => isLoading = true);
@@ -51,17 +78,14 @@ class _PaginatedListState extends State<PaginatedList>
       lastComplete = querySnapshot.size != 0
           ? querySnapshot.docs[querySnapshot.size - 1]
           : lastComplete;
-    else
-      lastTodo = querySnapshot.size != 0
-          ? querySnapshot.docs[querySnapshot.size - 1]
-          : lastTodo;
     querySnapshot.docs.forEach((element) {
       Todo temp = Todo(
           title: element['title'],
           desc: element['desc'],
           completed: element['completed'],
-          id: element.id);
-      todoList.add(temp);
+          id: element.id,
+          documentSnapshot: element);
+      PaginatedList.todoList.add(temp);
     });
     setState(() => isLoading = false);
   }
@@ -80,25 +104,40 @@ class _PaginatedListState extends State<PaginatedList>
     });
   }
 
+  setPaginatedViewState() {
+    setState(() {
+      List<Todo> todos = PaginatedList.todoList
+          .where((element) => element.completed == widget.completed)
+          .toList();
+      if (todos.length < limit) getTodos();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    List<Todo> displayList = [];
+    PaginatedList.todoList
+        .where((element) => element.completed == widget.completed)
+        .forEach((todo) => displayList.add(todo));
     return Column(
       children: [
         Expanded(
-          child: todoList.length == 0
+          child: displayList.length == 0
               ? Center(
                   child: Text('No to-dos'),
                 )
               : ListView.separated(
                   controller: _scrollController,
-                  itemCount: todoList.length,
+                  itemCount: displayList.length,
                   separatorBuilder: (context, index) => Container(
                     height: 5,
                   ),
                   itemBuilder: (context, index) {
-                    Todo todo = todoList[index];
-                    return TodoWidget(todo: todo);
+                    Todo todo = displayList[index];
+                    return TodoWidget(
+                        todo: todo,
+                        setPaginatedViewState: setPaginatedViewState);
                   },
                 ),
         ),
