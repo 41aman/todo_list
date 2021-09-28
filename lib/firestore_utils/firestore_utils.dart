3e987f5261928f755165736d5848ca84ac39403e
41aman <41aman.p@gmail.com>
2021-09-29 03:16:03 +0530

    firestore_utils: Modified functions to support pagination.
    Some of the CRUD functions were made async to await the completion of the operation and then update the list.

--- lib/firestore_utils/firestore_utils.dart
+++ lib/firestore_utils/firestore_utils.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_list/firestore_utils/paginated_view.dart';
 import 'package:todo_list/models/todo.dart';
 import 'package:todo_list/screens/todowidget.dart';
 
@@ -16,95 +17,40 @@ class FirestoreUtils {
       .snapshots();
 
   static Future<void> addTodo(Todo todo) {
-    return _todos
-        .add({
-          'created_time': FieldValue.serverTimestamp(),
-          'title': todo.title,
-          'desc': todo.desc,
-          'completed': todo.completed,
-        })
-        .then((value) {})
-        .catchError((error) => print(error));
+    return _todos.add({
+      'created_time': FieldValue.serverTimestamp(),
+      'title': todo.title,
+      'desc': todo.desc,
+      'completed': todo.completed,
+    }).then((value) async {
+      todo.documentSnapshot = await value.get();
+      todo.id = value.id;
+      PaginatedList.addTodo(todo);
+    }).catchError((error) => print(error));
   }
 
-  static Widget buildList(BuildContext context, bool completed) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: completed ? collectionStreamCompleted : collectionStreamTodo,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text(
-              'Something went wrong',
-              style: TextStyle(
-                color: Colors.red,
-              ),
-            );
-          }
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return Container(
-              color: Colors.white,
-              child: Center(
-                  child: CircularProgressIndicator(
-                color: Colors.blue,
-              )),
-            );
-          final List<Todo> todoList = [];
-          snapshot.data!.docs.forEach((element) {
-            Todo temp = Todo(
-              title: element['title'],
-              completed: element['completed'],
-              desc: element['desc'],
-              id: element.id,
-            );
-            todoList.add(temp);
-          });
-          return todoList.isEmpty
-              ? Center(
-                  child: Text(
-                    completed ? 'No To-dos' : 'No completed To-dos',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                )
-              : ListView.separated(
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    final todo = todoList[index];
-                    return TodoWidget(todo: todo);
-                  },
-                  separatorBuilder: (context, index) => Container(
-                    height: 5,
-                  ),
-                  itemCount: todoList.length,
-                );
-        });
-  }
-
-  static Future<void> toggleTodo(Todo todo) {
+  static Future<void> toggleTodo(Todo todo) async {
     bool completed = todo.completed;
-    return _todos
-        .doc(todo.id)
-        .update({'completed': !completed})
-        .then((value) => print("update successful"))
-        .catchError((error) => print(error));
+    await _todos.doc(todo.id).update({'completed': !completed}).then((value) {
+      print("update successful");
+      PaginatedList.toggleTodo(todo.id);
+    }).catchError((error) => print(error));
   }
 
-  static Future<void> removeTodo(String id) {
-    return _todos
-        .doc(id)
-        .delete()
-        .then((value) => print('delete successful'))
-        .catchError((error) => print(error));
+  static Future<void> removeTodo(String id) async {
+    await _todos.doc(id).delete().then((value) {
+      print('delete successful');
+      PaginatedList.removeTodo(id);
+    }).catchError((error) => print(error));
   }
 
-  static Future<void> editTodo(Todo todo) {
-    return _todos
-        .doc(todo.id)
-        .update({
-          'title': todo.title,
-          'desc': todo.desc,
-        })
-        .then((value) => print('edit successful'))
-        .catchError((error) => print(error));
+  static Future<void> editTodo(Todo todo) async {
+    await _todos.doc(todo.id).update({
+      'title': todo.title,
+      'desc': todo.desc,
+    }).then((value) {
+      print('edit successful');
+      PaginatedList.editTodo(todo);
+    }).catchError((error) => print(error));
   }
 }
